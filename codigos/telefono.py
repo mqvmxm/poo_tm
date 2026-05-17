74e782206f4d27a71867474ca2c93cde0b4cbe80
Tere Marroquín <1725110143@utectulancingo.edu.mx>
2026-05-17 22:25:13 +0000

    UPDATED gitignore

---
+++ codigos/telefono.py
@@ -0,0 +1,25 @@
+class Telefono:
+    def __init__(self, modelo, capacidad, color, porcentaje_bateria, operador, cantidad_camaras, numero_serie, espacio_usado, version, precio):
+        self.modelo = modelo
+        self.capacidad = capacidad
+        self.color = color
+        self.porcentaje_bateria = porcentaje_bateria
+        self.operador = operador
+        self.cantidad_camaras = cantidad_camaras
+        self.numero_serie = numero_serie
+        self.espacio_usado = espacio_usado
+        self.version = version
+        self.precio = precio
+
+        print(f"El modelo es: {self.modelo}")
+        print(f"La capacidad es de: {self.capacidad}")
+        print(f"El color es: {self.color}")
+        print(f"Porcentaje de bateria {self.porcentaje_bateria}")
+        print(f"Operador: {self.operador}")
+        print(f"Cantidad de Cámaras: {self.cantidad_camaras}")
+        print(f"Número de serie: {self.numero_serie}")
+        print(f"Espacio usado: {self.espacio_usado}")
+        print(f"Versión Actual: {self.version}")
+        print(f"Precio: ${self.precio}")
+
+mi_iphone = Telefono("Iphone 13", "128 GB", "Azul", "74%", "TELCEL", "3", "WYKLQ7MV90", "110.29 GB", "iOS 26.4.2", "14,000")
