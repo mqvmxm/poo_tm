d8d6be0e94da8b80b42426cdc9afd5a4de342713
Tere Marroquín <1725110143@utectulancingo.edu.mx>
2026-05-20 01:13:19 +0000

    CREATED gitignore

---
+++ codigos/perro.py
@@ -0,0 +1,40 @@
+class Perro:
+    def __init__(self, nombre, raza, edad, peso, color_pelaje, tipo_pelo, genero, es_estirilizado, temperamento, alergias):
+        self.nombre = nombre
+        self.raza = raza
+        self.edad = edad
+        self.peso = peso
+        self.color_pelaje = color_pelaje
+        self.tipo_pelo = tipo_pelo
+        self.genero = genero
+        self.es_esterilizado = es_estirilizado
+        self.temperamento = temperamento
+        self.alergias = alergias
+
+        print(f"Nombre: {self.nombre}")
+        print(f"Raza: {self.raza}")
+        print(f"Edad: {self.edad}")
+        print(f"Peso: {self.peso}")
+        print(f"Color de pelo: {self.color_pelaje}")
+        print(f"Tipo de pelo: {self.tipo_pelo}")
+        print(f"Género: {self.genero}")
+        print(f"¿Está esterilizado? {self.es_esterilizado}")
+        print(f"Temperamento: {self.temperamento}")
+        print(f"¿Tiene alergias? {self.alergias}")
+
+    def morder(self):
+        print("Método: El perro ha mordido")
+
+    def ladrar(self):
+        print("Método: El perro ha ladrado")
+
+    def correr(self):
+        print("Método: El perro está corriendo")
+
+    def jugar(self):
+        print("Método: El perro quiere jugar")
+
+    def comer(self):
+        print("Método: El perro quiere comer")
+
+mi_perro = Perro("Zansón", "Pastor Alemán", "9 años", "35 kg", "Negro con Marrón", "Corto", "Macho", False, "Agresivo", False)
