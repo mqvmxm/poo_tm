2497d0c53972a96888f25f76e9a0a6426cf909de
Tere Marroquín <1725110143@utectulancingo.edu.mx>
2026-05-20 03:24:52 +0000

    FIXED gitignore

--- codigos/mesa.py
+++ codigos/mesa.py
@@ -1,9 +1,9 @@
 class Mesa:
-    def __init__(self, material, ancho, alto, largo, forma, color, soporte, peso, capacidad, estado):
+    def __init__(self, material, ancho, alto, estilo, forma, color, soporte, peso, capacidad, estado):
         self.material = material
         self.ancho = ancho
         self.alto = alto
-        self.largo = largo
+        self.estilo = estilo
         self.forma = forma
         self.color = color 
         self.soporte = soporte
@@ -14,7 +14,7 @@ class Mesa:
         print(f"Material: {self.material}")
         print(f"Ancho: {self.ancho}")
         print(f"Alto: {self.alto}")
-        print(f"Largo: {self.largo}")
+        print(f"Estilo: {self.estilo}")
         print(f"Forma: {self.forma}")
         print(f"Color: {self.color}")
         print(f"Soporte: {self.soporte}")
@@ -37,6 +37,7 @@ class Mesa:
     def rayarSuperficie(self):
         print("Método: Se ha rayado la superficie de la mesa")
 
-mi_mesa = Mesa("Madera", "80cm", "75cm", "140cm", "Rectangular", "Café", "4 patas de madera", "25kg", "80 kg", "Nueva")
+mi_mesa = Mesa("Madera", "80cm", "75cm", "Vintage", "Rectangular", "Café", "4 patas de madera", "25kg", "80 kg", "Nueva")
+
 
 
