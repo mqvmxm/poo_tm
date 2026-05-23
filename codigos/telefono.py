554556ab6e7f4c2fc06bc75ca357ca53237fbdf6
Tere Marroquín <1725110143@utectulancingo.edu.mx>
2026-05-23 02:00:38 +0000

    UPDATED gitignore

--- codigos/telefono.py
+++ codigos/telefono.py
@@ -22,19 +22,44 @@ class Telefono:
         print(f"Versión Actual: {self.version}")
         print(f"Precio: ${self.precio}")
 
-    def sacarFotografias(self):
+    def sacarFotografias(self) -> None:
+        """
+        El usuario toma una fotografía con la cámara del celular
+
+        Este método no recibe argumentos adicionales ni hace return hacia otro valor
+        """
         print("Método: El teléfono ha sacado fotografías")
 
-    def hacerLlamada(self):
+    def hacerLlamada(self) -> None:
+        """
+        El usuario realiza una llamada telefónica 
+
+        Este método no recibe argumentos adicionales ni hace return hacia otro valor
+        """
         print("Método: El teléfono está haciendo llamada")
 
-    def abrirAplicacion(self):
+    def abrirAplicacion(self) -> None:
+        """
+        El usuario realiza la acción de abrir diferentes aplicaciones
+
+        Este método no recibe argumentos adicionales ni hace return hacia otro valor
+        """
         print("Método: El teléfono puede abrir diferentes aplicaciones")
 
-    def recibirNotificaciones(self):
+    def recibirNotificaciones(self) -> None:
+        """
+        El teléfono manda diferentes tipos de notificaciones
+
+        Este método no recibe argumentos adicionales ni hace return hacia otro valor
+        """
         print("Método: El teléfono recibe muchas notificaciones")
 
-    def apagar(self):
+    def apagar(self) -> None:
+        """
+        El teléfono o el usuario pueden apagar el teléfono 
+
+        Este método no recibe argumentos adicionales ni hace return hacia otro valor
+        """
         print("Método: El teléfono se apagó")
 
 mi_iphone = Telefono("Iphone 13", "128 GB", "Azul", "74%", "TELCEL", "3", "WYKLQ7MV90", "110.29 GB", "iOS 26.4.2", "14,000")
