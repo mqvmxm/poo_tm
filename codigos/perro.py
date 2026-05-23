85478ff7ad9e20227dbefb8b8b77c4a7c8e3757a
Tere Marroquín <1725110143@utectulancingo.edu.mx>
2026-05-23 01:28:48 +0000

    UPDATED gitignore

--- codigos/perro.py
+++ codigos/perro.py
@@ -22,19 +22,44 @@ class Perro:
         print(f"Temperamento: {self.temperamento}")
         print(f"¿Tiene alergias? {self.alergias}")
 
-    def morder(self):
+    def morder(self) -> None:
+        """
+        Simula la acción en la que el perro muerde x objeto
+
+        Este método no recibe argumentos adicionales ni hace return hacia otro valor
+        """
         print("Método: El perro ha mordido")
 
-    def ladrar(self):
+    def ladrar(self) -> None:
+        """
+        Simula que el perro emite un ladrido
+
+        Este método no recibe argumentos adicionales ni hace return hacia otro valor
+        """
         print("Método: El perro ha ladrado")
 
-    def correr(self):
+    def correr(self) -> None:
+        """
+        Simula que el perro se desplaza corriendo
+
+        Este método no recibe argumentos adicionales ni hace return hacia otro valor
+        """
         print("Método: El perro está corriendo")
 
-    def jugar(self):
+    def jugar(self) -> None:
+        """
+        Simula que el perro se encuentra jugando
+
+        Este método no recibe argumentos adicionales ni hace return hacia otro valor
+        """
         print("Método: El perro quiere jugar")
 
-    def comer(self):
+    def comer(self) -> None:
+        """
+        Simula el momento en el que el perro consume sus alimentos
+
+        Este método no recibe argumentos adicionales ni hace return hacia otro valor
+        """
         print("Método: El perro quiere comer")
 
 mi_perro = Perro("Zansón", "Pastor Alemán", "9 años", "35 kg", "Negro con Marrón", "Corto", "Macho", False, "Agresivo", False)
